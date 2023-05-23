5e363e3781cbe4f18c876f80c79c4d4734b30c03
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-23 15:46:53 +0200

    Backend: Add Create, Delete and Update methods to UserRepository

--- Backend/Repositories/IUserRepository.cs
+++ Backend/Repositories/IUserRepository.cs
@@ -5,7 +5,10 @@ namespace Backend.Repositories
     public interface IUserRepository
     {
         public Task<List<User>> GetAll();
+        public Task<User> Create(User user);
         public Task<User?> Find(int id);
         public Task<bool> IsUnique(string email);
+        public Task<bool> Delete(User user);
+        public Task<bool> Update(User user);
     }
 }
--- Backend/Repositories/UserRepository.cs
+++ Backend/Repositories/UserRepository.cs
@@ -13,10 +13,30 @@ namespace Backend.Repositories
         }
 
         public async Task<List<User>> GetAll() => await _context.Users.AsNoTracking().ToListAsync();
-        
+
+        public async Task<User> Create(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
         public async Task<User?> Find(int id) => await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
 
         public async Task<bool> IsUnique(string email) => await _context.Users.AllAsync(u => u.Email != email);
 
+        public async Task<bool> Delete(User user)
+        {
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Update(User user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
