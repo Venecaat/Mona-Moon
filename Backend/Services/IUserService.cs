4e3bc051124cc256866699cedfbb89469ca3b0db
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-23 16:57:39 +0200

    Backend: Add Create, IsUnique, Delete and Update methods to UserService

--- Backend/Services/IUserService.cs
+++ Backend/Services/IUserService.cs
@@ -1,12 +1,15 @@
-﻿using Backend.Database.Models.User;
-using Backend.Dtos.User;
+﻿using Backend.Dtos.User;
 
 namespace Backend.Services
 {
     public interface IUserService
     {
         public Task<List<PublicUser>> GetAll();
+        public Task<PublicUser> Create(RegisterUser newUser);
         public Task<PublicUser?> Find(int id);
         public Task<PublicUser?> FindByEmail(string email);
+        public Task<bool> IsUnique(string email);
+        public Task<bool> Delete(int id);
+        public Task<PublicUser> Update(PublicUser user);
     }
 }
--- Backend/Services/UserService.cs
+++ Backend/Services/UserService.cs
@@ -17,6 +17,13 @@ namespace Backend.Services
 
         public async Task<List<PublicUser>> GetAll() => _mapper.Map<List<User>, List<PublicUser>>(await _repository.GetAll());
 
+        public async Task<PublicUser> Create(RegisterUser newUser)
+        {
+            User user = _mapper.Map<RegisterUser, User>(newUser);
+             await _repository.Create(user);
+            return _mapper.Map<User, PublicUser>(user);
+        }
+
         public async Task<PublicUser?> Find(int id)
         {
             User? user = await _repository.Find(id);
@@ -27,6 +34,26 @@ namespace Backend.Services
         {
             User? user = await _repository.FindByEmail(email);
             return user == null ? null : _mapper.Map<User, PublicUser>(user);
+        }
+
+        public async Task<bool> IsUnique(string email) => await _repository.IsUnique(email);
+
+        public async Task<bool> Delete(int id)
+        {
+            User? user = await _repository.Find(id);
+
+            if (user == null) return false;
+
+            await _repository.Delete(user);
+            return true;
+        }
+
+        public async Task<PublicUser> Update(PublicUser updateUser)
+        {
+            User user = _mapper.Map<PublicUser, User>(updateUser);
+            await _repository.Update(user);
+
+            return updateUser;
         }
     }
 }
