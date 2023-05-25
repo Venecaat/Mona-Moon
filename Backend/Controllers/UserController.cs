9a2afcec9f43db5c1fe0892a6dba88ab473f8516
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-25 11:24:54 +0200

    Backend: Add GetAllUser and GetUserById endpoints in UserController

--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Backend.Dtos.User;
+﻿using Backend.Database.Models.User;
+using Backend.Dtos.User;
+using Backend.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -7,16 +10,45 @@ namespace Backend.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<List<PublicUser>>> GetAllUser()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<PublicUser> users = await _service.GetAll();
+                return StatusCode(StatusCodes.Status200OK, users);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Oops, something went wrong!");
+            }
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<PublicUser>> GetUserById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                PublicUser? user = await _service.Find(id);
+                return user is null ? StatusCode(StatusCodes.Status404NotFound, $"There is no user with this Id: {id}")
+                    : StatusCode(StatusCodes.Status200OK, user);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Oops, something went wrong!");
+            }
         }
 
         [HttpPost]
