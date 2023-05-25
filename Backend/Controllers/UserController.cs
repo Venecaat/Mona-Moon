6acdfc72cde66796cc7811beb8a983a3f440180a
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-25 15:43:35 +0200

    Backend: Add CreateUser, UpdateUser and DeleteUser endpoints in UserController

--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -42,7 +42,7 @@ namespace Backend.Controllers
             try
             {
                 PublicUser? user = await _service.Find(id);
-                return user is null ? StatusCode(StatusCodes.Status404NotFound, $"There is no user with this Id: {id}")
+                return user is null ? StatusCode(StatusCodes.Status404NotFound, $"There is no user with this Id: {id}!")
                     : StatusCode(StatusCodes.Status200OK, user);
             }
             catch
@@ -52,21 +52,67 @@ namespace Backend.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public async Task<ActionResult<PublicUser>> CreateUser(RegisterUser newUser)
         {
-            throw new NotImplementedException();
+            PublicUser? existingUser = await _service.FindByEmail(newUser.Email);
+
+            if (existingUser is not null) return StatusCode(StatusCodes.Status409Conflict, $"This Email address is already taken: {newUser.Email}!");
+
+            try
+            {
+                PublicUser user = await _service.Create(newUser);
+                return StatusCode(StatusCodes.Status201Created, user);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Oops, something went wrong!");
+            }
         }
 
-        [HttpPut("{id}")]
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public async Task<ActionResult<PublicUser>> UpdateUser(UpdateUser updateUser)
         {
-            throw new NotImplementedException();
+            PublicUser? existingUser = await _service.FindByEmail(updateUser.Email);
+
+            if (existingUser is not null) return StatusCode(StatusCodes.Status409Conflict, $"This Email address is already taken: {updateUser.Email}!");
+
+            existingUser = await _service.Find(updateUser.Id);
+
+            if (existingUser is null) return StatusCode(StatusCodes.Status404NotFound, $"There is no user with this Id: {updateUser.Id}!");
+
+            try
+            {
+                PublicUser user = await _service.Update(updateUser);
+                return StatusCode(StatusCodes.Status200OK, user);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Oops, something went wrong!");
+            }
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> DeleteUser(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _service.Delete(id) ? StatusCode(StatusCodes.Status200OK, $"The user with this id: {id} has been successfully deleted!")
+                    : StatusCode(StatusCodes.Status404NotFound, $"There is no user with this Id: {id}!");
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Oops, something went wrong!");
+            }
         }
     }
 }
