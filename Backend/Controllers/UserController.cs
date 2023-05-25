60d055790de1bace46f08282b3eb9fd231f2aaa9
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-25 10:26:02 +0200

    Backend: Add UserController

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<PublicUser>>> GetAllUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PublicUser>> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PublicUser>> CreateUser(RegisterUser newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PublicUser>> UpdateUser(UpdateUser updateUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
