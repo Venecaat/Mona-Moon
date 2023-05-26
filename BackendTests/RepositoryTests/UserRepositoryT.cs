2ec51683ac3c646028c7bd69ace94828c4caad6e
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-26 18:05:17 +0200

    Backend: Add NUnit test project

---
+++ BackendTests/RepositoryTests/UserRepositoryT.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace BackendTests.RepositoryTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ BackendTests/ServiceTests/UserServiceT.cs
@@ -0,0 +1,20 @@
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace BackendTests.ServiceTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
