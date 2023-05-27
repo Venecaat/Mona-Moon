1af0deab58be178cefe62713584b67404dbb6c36
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-27 20:22:58 +0200

    Backend: Update methods with virtual keyword for mocking

--- Backend/Repositories/UserRepository.cs
+++ Backend/Repositories/UserRepository.cs
@@ -12,29 +12,29 @@ namespace Backend.Repositories
             _context = context;
         }
 
-        public async Task<List<User>> GetAll() => await _context.Users.AsNoTracking().ToListAsync();
+        public virtual async Task<List<User>> GetAll() => await _context.Users.AsNoTracking().ToListAsync();
 
-        public async Task<User> Create(User user)
+        public virtual async Task<User> Create(User user)
         {
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
         }
 
-        public async Task<User?> Find(int id) => await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
+        public virtual async Task<User?> Find(int id) => await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
 
-        public async Task<User?> FindByEmail(string email) => await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email);
+        public virtual async Task<User?> FindByEmail(string email) => await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email);
 
-        public async Task<bool> IsUnique(string email) => await _context.Users.AllAsync(u => u.Email != email);
+        public virtual async Task<bool> IsUnique(string email) => await _context.Users.AllAsync(u => u.Email != email);
 
-        public async Task<bool> Delete(User user)
+        public virtual async Task<bool> Delete(User user)
         {
             _context.Users.Remove(user);
             await _context.SaveChangesAsync();
             return true;
         }
 
-        public async Task<bool> Update(User user)
+        public virtual async Task<bool> Update(User user)
         {
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
