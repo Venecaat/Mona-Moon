d7524b03fb5e289325711552ad6e1ca8a38077f9
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-27 17:35:31 +0200

    BackendTests: Add tests for Create method in UserRepositoryT

--- BackendTests/RepositoryTests/UserRepositoryT.cs
+++ BackendTests/RepositoryTests/UserRepositoryT.cs
@@ -67,5 +67,63 @@ namespace BackendTests.RepositoryTests
         {
             Util.AreEqualByJson(users, await repository.GetAll());
         }
+
+        [Test]
+        public async Task Create_ReturnsNewUser()
+        {
+            User expectedUser = new ()
+            {
+                Id = 0,
+                Email = "pinpon@yeah.com",
+                FirstName = "Jenny",
+                LastName = "Walsh",
+                Password = "Gotcha1337",
+                IsAdmin = false
+            };
+
+            User newUser = await repository.Create(expectedUser);
+
+            Util.AreEqualByJson(expectedUser, newUser);
+        }
+
+        [Test]
+        public async Task Create_AddsNewUserToDb()
+        {
+            User newUser = new()
+            {
+                Id = 0,
+                Email = "electro@tenshukaku.ina",
+                FirstName = "Raiden",
+                LastName = "Shogun",
+                Password = "eternity",
+                IsAdmin = false
+            };
+
+            mockContext.Setup(x => x.Users.AddAsync(It.IsAny<User>(), default))
+                .Callback<User, CancellationToken>((u, _) => { users.Add(u); });
+
+            await repository.Create(newUser);
+
+            Assert.That(users.Count, Is.EqualTo(6));
+        }
+
+        [Test]
+        public async Task Create_AddAsyncAndSaveChangesAsyncMethodsCalledOnDb()
+        {
+            User newUser = new()
+            {
+                Id = 0,
+                Email = "electro@tenshukaku.ina",
+                FirstName = "Raiden",
+                LastName = "Shogun",
+                Password = "eternity",
+                IsAdmin = false
+            };
+
+            await repository.Create(newUser);
+
+            mockContext.Verify(x => x.Users.AddAsync(It.IsAny<User>(), It.IsAny<CancellationToken>()));
+            mockContext.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()));
+        }
     }
 }
