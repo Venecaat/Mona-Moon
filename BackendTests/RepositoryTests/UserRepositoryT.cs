4887638144ba75ad4ea7a2a55f11b4dddc5f2919
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-27 17:16:47 +0200

    BackendTests: Add setup and test for GetAll method in UserRepositoryT

--- BackendTests/RepositoryTests/UserRepositoryT.cs
+++ BackendTests/RepositoryTests/UserRepositoryT.cs
@@ -1,20 +1,71 @@
-﻿using Moq;
+﻿using Backend.Database;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using Moq.EntityFrameworkCore;
+using Backend.Database.Models.User;
+using Backend.Repositories;
 
 namespace BackendTests.RepositoryTests
 {
     [TestFixture]
-    public class Tests
+    public class UserRepositoryT
     {
+        private Mock<MonaMoonDbContext> mockContext;
+        private List<User> users;
+        private UserRepository repository;
+
         [SetUp]
         public void Setup()
         {
+            users = new()
+            {
+                new ()
+                {
+                    Email = "totally@valid.com",
+                    FirstName = "Johnson",
+                    LastName = "Long",
+                    Password = "blackCat"
+                },
+                new ()
+                {
+                    Email = "something@right.bs",
+                    FirstName = "Jelly",
+                    LastName = "Fish",
+                    Password = "artifacto"
+                },
+                new ()
+                {
+                    Email = "marker123@free.net",
+                    FirstName = "Isaac",
+                    LastName = "Clarke",
+                    Password = "usgishimura"
+                },
+                new ()
+                {
+                    Email = "tryingFor@you.com",
+                    FirstName = "Yuki",
+                    LastName = "Onna",
+                    Password = "snowbutterfly"
+                },
+                new ()
+                {
+                    Email = "lastsamurai325@liamg.com",
+                    FirstName = "Oda",
+                    LastName = "Nobunaga",
+                    Password = "harakiri"
+                }
+            };
+
+            mockContext = new(new DbContextOptions<MonaMoonDbContext>());
+            mockContext.Setup(x => x.Users).ReturnsDbSet(users);
+
+            repository = new UserRepository(mockContext.Object);
         }
 
         [Test]
-        public void Test1()
+        public async Task GetAll_WhenCalled_ReturnsUsersList()
         {
-            Assert.Pass();
+            Util.AreEqualByJson(users, await repository.GetAll());
         }
     }
 }
