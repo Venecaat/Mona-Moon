c6ae83cd6bc0f18b6e35d593200c80bc6810f71c
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-05-31 19:27:52 +0200

    Frontend: Add MainPage.jsx and Route for it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import { MainPage } from "./routes/MainPage.jsx";
 import { PageNotFound } from "./routes/PageNotFound.jsx";
 import { Layout } from "./components/Layout.jsx";
-import './App.css'
 
 export const App = () => {
 
   return (
       <Layout>
           <Routes>
+              <Route path="/" element={<MainPage />}></Route>
               <Route path="*" element={<PageNotFound />}></Route>
           </Routes>
       </Layout>
