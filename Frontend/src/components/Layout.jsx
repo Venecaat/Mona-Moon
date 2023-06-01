dd96c36f3de5d2c5ab3ed726d46f171183d90ff1
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-06-01 14:33:04 +0200

    Frontend: Add Footer component

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
+import {Footer} from "./Footer.jsx";
 
 export const Layout = ({ children }) => {
     return (
         <div>
-            <div>Navbar</div>
+            <div className="text-neutral w-6/12 border-2 text-center font-bold text-3xl p-2 border-accent-content bg-info">Navbar</div>
                 <main>
                     { children }
                 </main>
-            <div>Footer</div>
+            <Footer />
         </div>
     )
 }
