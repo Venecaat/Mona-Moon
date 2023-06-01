26aa24c4c94de6c76b93915613f370918bc63850
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-06-01 18:32:55 +0200

    Frontend: Add Navbar component

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+export const Navbar = () => {
+    return (
+        <div className="navbar bg-primary text-primary-content">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h8m-8 6h16"/>
+                        </svg>
+                    </label>
+                    <ul tabIndex="0"
+                        className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-52 font-bold z-10">
+                        <li><a>Órarend</a></li>
+                        <li><a>Bemutatkozás</a></li>
+                        <li><a>Kapcsolat</a></li>
+                    </ul>
+                </div>
+                <a className="hidden lg:btn lg:btn-ghost lg:normal-case lg:text-xl">Mona Moon</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-lg font-bold">
+                    <li><a>Órarend</a></li>
+                    <li><a>Bemutatkozás</a></li>
+                    <li><a>Kapcsolat</a></li>
+                </ul>
+            </div>
+            <div className="navbar-center lg:hidden flex">
+                <a className="btn btn-ghost normal-case text-xl">Mona Moon</a>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Bejelentkezés</a>
+            </div>
+        </div>
+    )
+}
