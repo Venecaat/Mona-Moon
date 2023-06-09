d0b7099f446a318427ea47644430a98ae2869786
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-06-09 20:07:08 +0200

    Frontend: Add ServiceCard component for Main Page

---
+++ Frontend/src/components/mainpage/ServiceCard.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+export const ServiceCard = ( props ) => {
+    return (
+        <div className="card card-compact w-96 bg-primary shadow-xl">
+            <figure><img src={ props.imageLink } alt="Shoes" /></figure>
+            <div className="card-body text-center">
+                <a className="underline underline-offset-4 lg:no-underline text-xl font-semibold text-secondary hover:text-accent hover:cursor-pointer">
+                    { props.linkTitle }
+                </a>
+            </div>
+        </div>
+    )
+}
+
+ServiceCard.propTypes = {
+    imageLink: PropTypes.string,
+    linkTitle: PropTypes.string
+};
