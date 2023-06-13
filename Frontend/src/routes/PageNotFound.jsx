11849369aa5169da4f4837655bd6e8fbdf6b6644
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-06-13 15:57:24 +0200

    Frontend: Make page for page not found

--- Frontend/src/routes/PageNotFound.jsx
+++ Frontend/src/routes/PageNotFound.jsx
@@ -1,6 +1,13 @@
 
 export const PageNotFound = () => {
     return (
-        <div>Error 404 - Page Not Found</div>
+        <div>
+            <div className="text-center alert alert-error rounded-none lg:rounded-box">
+                <div className="py-4">
+                    <h1 className="text-7xl font-bold mb-5">404</h1>
+                    <h1 className="text-5xl font-bold">A keresett oldal nem található!</h1>
+                </div>
+            </div>
+        </div>
     )
 }
