8e5b852d20cb8feaa92b594f580e4ff2e9621b30
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-06-22 19:29:25 +0200

    Frontend: Styling

--- Frontend/src/components/mainpage/ServiceCard.jsx
+++ Frontend/src/components/mainpage/ServiceCard.jsx
@@ -5,7 +5,7 @@ export const ServiceCard = ( props ) => {
         <div className="card card-compact w-96 bg-primary shadow-xl">
             <figure><img src={ props.imageLink } alt="Shoes" /></figure>
             <div className="card-body text-center">
-                <a className="underline underline-offset-4 lg:no-underline text-xl font-semibold text-secondary hover:text-accent hover:cursor-pointer">
+                <a className="underline underline-offset-4 lg:no-underline text-2xl font-semibold text-secondary hover:text-accent hover:cursor-pointer">
                     { props.linkTitle }
                 </a>
             </div>
