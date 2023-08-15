b69719a9f07abb33cb4acf9a5393edaadf53143f
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-08-15 16:10:03 +0200

    Frontend: Styling; Add menu for account related things in Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <div className="navbar bg-primary text-primary-content">
+        <div className="navbar bg-primary text-primary-content max-h-12">
+            {/* TABLET/MOBILE MENU */}
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M4 6h16M4 12h8m-8 6h16"/>
@@ -32,8 +33,8 @@ export const Navbar = () => {
                                         <details>
                                             <summary>Telihold Anyaoltalom</summary>
                                             <ul className="text-secondary">
-                                                <li><a>Fitmommy várandós hastánc</a></li>
-                                                <li><a>Fitmommy várandós latin fitness</a></li>
+                                                <li><a>Fitmummy várandós hastánc</a></li>
+                                                <li><a>Fitmummy várandós latin fitness</a></li>
                                                 <li><a>Dúlai támogatás</a></li>
                                                 <li><a>Gyermekágyas gondoskodás</a></li>
                                                 <li><a>Császármetszés feldolgozó csoport</a></li>
@@ -59,6 +60,8 @@ export const Navbar = () => {
                 </div>
                 <Link to="/" className="hidden lg:btn lg:btn-ghost lg:normal-case lg:text-xl">Mona Moon</Link>
             </div>
+
+            {/* NORMAL MENU */}
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-lg font-bold">
                     <li className="dropdown dropdown-hover">
@@ -78,8 +81,8 @@ export const Navbar = () => {
                                 <details>
                                     <summary>Telihold Anyaoltalom</summary>
                                     <ul className="text-secondary">
-                                        <li className="hover:text-accent"><a>Fitmommy várandós hastánc</a></li>
-                                        <li className="hover:text-accent"><a>Fitmommy várandós latin fitness</a></li>
+                                        <li className="hover:text-accent"><a>Fitmummy várandós hastánc</a></li>
+                                        <li className="hover:text-accent"><a>Fitmummy várandós latin fitness</a></li>
                                         <li className="hover:text-accent"><a>Dúlai támogatás</a></li>
                                         <li className="hover:text-accent"><a>Gyermekágyas gondoskodás</a></li>
                                         <li className="hover:text-accent"><a>Császármetszés feldolgozó csoport</a></li>
@@ -100,11 +103,25 @@ export const Navbar = () => {
                     <li><Link to="/kapcsolat">Kapcsolat</Link></li>
                 </ul>
             </div>
+
             <div className="navbar-center lg:hidden flex">
                 <Link to="/" className="btn btn-ghost normal-case text-xl">Mona Moon</Link>
             </div>
+
             <div className="navbar-end">
-                <a className="btn">Bejelentkezés</a>
+                <div className="dropdown dropdown-end dropdown-hover">
+                    <label tabIndex="0" className="btn btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 lg:h-7 lg:w-7 text-primary-content" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3"/>
+                            <circle cx="12" cy="10" r="3"/><circle cx="12" cy="12" r="10"/>
+                        </svg>
+                    </label>
+                    <ul className="menu dropdown-content mt-3 lg:mt-0 p-2 z-10 w-40 shadow bg-primary font-bold rounded-box">
+                        <li><Link to="/bejelentkezes">Bejelentkezés</Link></li>
+                        <li><Link to="/regisztracio">Regisztráció</Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
--- Frontend/src/routes/services/bellydance-zumba/BellyDancePerformance.jsx
+++ Frontend/src/routes/services/bellydance-zumba/BellyDancePerformance.jsx
@@ -5,7 +5,7 @@ export const BellyDancePerformance = () => {
             <div className="bg-base">
                 <div className="hero-content flex-col md:flex-row gap-0 lg:gap-4">
                     <div>
-                        <h1 className="text-6xl font-bold text-center md:text-left text-primary">Engedd, hogy a kelet misztikus világa magával ragadjon</h1>
+                        <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left text-primary">Engedd, hogy a kelet misztikus világa magával ragadjon</h1>
                     </div>
                     <img src="/src/assets/images/hastanc-fellepes.jpg" className="max-w-sm shadow-2xl scale-90 lg:scale-100" />
                 </div>
