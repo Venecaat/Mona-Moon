b76c46484ccb4c5fdfe0bb01d8dc5b0dcc6b71dd
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-08-19 15:23:56 +0200

    Frontend: Add page for Zumba lessons

---
+++ Frontend/src/routes/services/bellydance-zumba/ZumbaLesson.jsx
@@ -0,0 +1,69 @@
+
+export const ZumbaLesson = () => {
+    return (
+        <div className="text-xl font-semibold">
+            <div className="bg-base">
+                <div className="hero-content flex-col md:flex-row gap-0 lg:gap-4">
+                    <div>
+                        <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left text-primary">Gyere és próbáld ki!</h1>
+                    </div>
+                    <img src="/src/assets/images/zumba-lesson.png" className="max-w-sm shadow-2xl scale-90 lg:scale-100" />
+                </div>
+            </div>
+            <hr className="w-11/12 xl:w-full mx-auto my-8 border-2 border-primary rounded" />
+            <div className="grid grid-cols-1 mb-6 text-center md:text-left text-2xl gap-7">
+                <div className="px-4 xl:px-0">
+                    <h2 className="text-primary font-bold text-3xl">Használt táncstílusok fitness mozdulatokkal kombinálva</h2>
+                    <ul className="md:ml-5 mt-3 italic grid grid-cols-1 gap-2">
+                        <li>Salsa</li>
+                        <li>Cumbia</li>
+                        <li>Reggaeton</li>
+                        <li>Merengue</li>
+                        <li>Hastánc</li>
+                        <li>Flamenco</li>
+                        <li>Bachata</li>
+                        <li>Calypso</li>
+                        <li>Samba</li>
+                        <li>Stb.</li>
+                    </ul>
+                </div>
+                <div className="px-4 xl:px-0 font-bold">
+                    Nem egy komplett táncstílust tanulunk meg, hanem csak táncolunk és jól érezzük magunkat!
+                </div>
+                <div className="px-4 xl:px-0">
+                    Ideje, hogy te is felfedezd azt a mozgásformát, ami neked a legjobb! Talán az én órámon találod meg, amit keresel. Várlak kortól és alkattól függetlenül!
+                </div>
+                <div className="px-4 xl:px-0">
+                    <h2 className="text-primary font-bold text-3xl">Az órák felépítése</h2>
+                    <ol className="md:ml-11 mt-3 italic grid grid-cols-1 gap-2 md:list-decimal">
+                        <li>Bemelegítés: 8-10 perc</li>
+                        <li>Koreográfiák: 40-45 perc, 8-10 zeneszám - havonta cserélek le egy számot, így követhető marad az óra, de mégsem lesz unalmas!</li>
+                        <li>Nyújtás: 5-10 perc</li>
+                    </ol>
+                </div>
+                <div className="px-4 xl:px-0">
+                    <h2 className="text-primary font-bold text-3xl">Milyen ruhában érdemes Zumba órára jönni?</h2>
+                    <ul className="md:ml-5 mt-3 italic grid grid-cols-1 gap-2">
+                        <li>Kényelmes nadrág, amiben könnyedén tudsz mozogni</li>
+                        <li>Kényelmes póló vagy top - lehetőleg rövid ujjú, mert meleged lesz</li>
+                        <li>Sportcipő, edzőcipő</li>
+                        <li>Mindenképpen szükséges legalább fél liter folyadék</li>
+                        <li>Esetleg egy kisméretű törülköző</li>
+                    </ul>
+                </div>
+                <div className="grid grid-cols-1 bg-primary p-5 text-warning xl:rounded-box text-center gap-4">
+                    <div>
+                        Az órákon saját felelősségre veszel részt.
+                    </div>
+                    <div>
+                        Amennyiben egészségügyi problémád, panaszod van, konzultálj kezelőorvosoddal.
+                        Az órákon való részvétel nem helyettesíti az orvossal, gyógytornásszal való konzultációt.
+                    </div>
+                    <div>
+                        A fentieket a részvételeddel elfogadottnak tekinted.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
