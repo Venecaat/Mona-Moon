3e15164453223e5ac2edee7c9e652248472a3b68
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-08-22 15:04:26 +0200

    Frontend: Make page for Birth Blessing

--- Frontend/src/routes/services/fullmoon-mp/BirthBlessing.jsx
+++ Frontend/src/routes/services/fullmoon-mp/BirthBlessing.jsx
@@ -1,8 +1,69 @@
+import { Link } from "react-router-dom";
 
 export const BirthBlessing = () => {
     return (
-        <div>
-            <h2>Szülésáldó ünnep</h2>
+        <div className="text-xl font-semibold">
+            <div className="bg-base">
+                <div className="hero-content flex-col md:flex-row gap-0 lg:gap-4">
+                    <div>
+                        <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left text-primary">Szülésáldó ünnep</h1>
+                    </div>
+                    <img src="/src/assets/images/birth-blessing.png" className="max-w-sm shadow-2xl scale-90 lg:scale-100" alt="Szülésáldó ünnep" />
+                </div>
+            </div>
+            <hr className="w-11/12 xl:w-full mx-auto my-8 border-2 border-primary rounded" />
+            <div className="grid grid-cols-1 mb-6 text-center md:text-left text-2xl gap-7">
+                <div className="px-4 xl:px-0">
+                    A szülésáldó, szülésre bocsájtó ünnep, azaz közismertebb nevén <span className="italic">Blessingway</span>,
+                    egy nap, ami <span className="font-bold">RÓLAD</span> szól, ami érted van,
+                    olyan női rokonok, barátok, hozzád közelálló nők részvételével, akik megtartanak,
+                    támogatnak, bátorítanak az anyává válás útján.
+                </div>
+                <div className="px-4 xl:px-0">
+                    Régebben fontos szerepet töltöttek be az emberek életében a rítusok, szertartások,
+                    amik mára már néhány kivételtől eltekintve igencsak kikoptak az életünkből.
+                    Pedig milyen fontos lenne egy-egy nagyobb életesemény előtt elbúcsúztatni a régi önmagunkat,
+                    és teret adni valami újnak, valami másnak. Bátorságot gyűjteni, elengedni a félelmeinket szeretteink körében
+                    egy-egy nagy lépés megtétele előtt.
+                </div>
+                <div className="px-4 xl:px-0">
+                    És mi lehetne nagyobb dolog az első, második, harmadik vagy akár többedik gyermekünk születésénél?
+                    Szerintem nem sok minden.... És nem is sokszor történik meg az életünk során.
+                </div>
+                <div className="px-4 xl:px-0">
+                    Ezért érdemes lehet megünnepelni gyönyörű virágokkal, gyertyákkal feldíszített közegben,
+                    ami lehet a szabadban, az otthonod melegében, vagy akár egy bérelt teremben a kedvenc "ünneplő" ruhádba öltözve.
+                    Meglátod mennyire jó érzés fogadni a szeretetet, odafigyelést, vagy akár egy kis kényeztetést.
+                    Pl: hajfonás, koszorúkészítés, masszázs, lábmosás.
+                </div>
+                <div className="px-4 xl:px-0">
+                    Az ünnep minden esetben a TE személyiségedhez, igényeidhez lesz igazítva.
+                    Bár általában vannak állandó elemei, mint például az áldásadás, fonálceremónia, útravaló ajándék.
+                </div>
+                <div className="px-4 xl:px-0">
+                    A szülés óráiban lesz majd mihez visszanyúlnod, az itt megtapasztalt megerősítések,
+                    pozitív energiák a tapasztalatok alapján sokszor erőt adnak a vajúdó anyának.
+                    Ha szeretnéd, veled lehetnek szülésed alatt az itt kapott ajándékok,
+                    amik szintén emlékeztetni tudnak erre a különleges napra.
+                </div>
+                <div className="px-4 my-4 xl:px-0 text-center text-3xl text-primary font-bold">
+                    Ha szeretnéd magadat, lányodat, barátnődet meglepni egy ősi gyökerekkel rendelkező,
+                    de abszolút modern szertartással Budapesten belül, akkor bátran fordulj hozzám!
+                </div>
+                <button type="button" className="btn w-3/4 md:w-1/2 mx-auto bg-secondary text-primary font-bold border-secondary text-2xl hover:bg-accent hover:border-accent">
+                    <Link to="/kapcsolat">Elérhetőségek</Link>
+                </button>
+                <div className="grid grid-cols-1 bg-primary p-5 text-info xl:rounded-box text-center text-lg gap-4">
+                    <div>
+                        Az ünnep időtartama általában 2-3 óra, ami a résztvevők számától függően változhat.
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 bg-primary p-5 text-warning xl:rounded-box text-center gap-4">
+                    <div>
+                        Az árazás több tényezőtől függ, ezért előzetes egyeztetés után tudok árajánlatot adni.
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
