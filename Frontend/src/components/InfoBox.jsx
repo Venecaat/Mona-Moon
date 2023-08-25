1305df59ead287c7cfd46f247502110cfc8390ce
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-08-25 10:29:29 +0200

    Frontend: Change the text size of the Info box

--- Frontend/src/components/InfoBox.jsx
+++ Frontend/src/components/InfoBox.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export const InfoBox = ({ children }) => {
     return (
-        <div className="grid grid-cols-1 bg-primary p-5 text-info xl:rounded-box text-center text-lg gap-4">
+        <div className="grid grid-cols-1 bg-primary p-5 text-info xl:rounded-box text-center text-2xl gap-4">
             { children }
         </div>
     )
