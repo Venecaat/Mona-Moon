4cd2c47f58a8065fec92759a1e0fb6f600db1147
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-06 14:19:53 +0200

    Frontend: Add option for extra classes in ULParagraph component

--- Frontend/src/components/ULParagraph.jsx
+++ Frontend/src/components/ULParagraph.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 
-export const ULParagraph = ({ title, children }) => {
+export const ULParagraph = ({ title, extraClasses, children }) => {
     return (
         <div className="px-4 xl:px-0">
             <h2 className="text-primary font-bold text-3xl">{ title }</h2>
-            <ul className="italic grid grid-cols-1 gap-2 mt-3 md:ml-5">
+            <ul className={"italic grid grid-cols-1 gap-2 mt-3 md:ml-5 " + extraClasses}>
                 { children }
             </ul>
         </div>
@@ -13,5 +13,6 @@ export const ULParagraph = ({ title, children }) => {
 
 ULParagraph.propTypes = {
     title: PropTypes.string,
+    extraClasses: PropTypes.string,
     children: PropTypes.any
 };
