e573a2e9da570b109cd3b3d01cc0c5c1afbcb0d2
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-06 14:46:05 +0200

    Frontend: Add gap prop, and default values to props in ULParagraph component

--- Frontend/src/components/ULParagraph.jsx
+++ Frontend/src/components/ULParagraph.jsx
@@ -1,18 +1,24 @@
 import PropTypes from "prop-types";
 
-export const ULParagraph = ({ title, extraClasses, children }) => {
+export const ULParagraph = ({ title, extraClasses, children, gap }) => {
     return (
         <div className="px-4 xl:px-0">
             <h2 className="text-primary font-bold text-3xl">{ title }</h2>
-            <ul className={"italic grid grid-cols-1 gap-2 mt-3 md:ml-5 " + extraClasses}>
+            <ul className={"italic grid grid-cols-1 gap-"+ gap + " mt-3 md:ml-5 " + extraClasses}>
                 { children }
             </ul>
         </div>
     )
 }
 
+ULParagraph.defaultProps = {
+    extraClasses: "",
+    gap: 2
+}
+
 ULParagraph.propTypes = {
     title: PropTypes.string,
     extraClasses: PropTypes.string,
-    children: PropTypes.any
+    children: PropTypes.any,
+    gap: PropTypes.number
 };
