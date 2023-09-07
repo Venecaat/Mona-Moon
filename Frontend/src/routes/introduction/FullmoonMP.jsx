6d28901ff5701e22b3eb1b1a09f16c9a2aa9ee8b
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-07 17:21:42 +0200

    Frontend: Change FullmoonMP introduction page to use components

--- Frontend/src/routes/introduction/FullmoonMP.jsx
+++ Frontend/src/routes/introduction/FullmoonMP.jsx
@@ -1,10 +1,11 @@
+import { NormalParagraph } from "../../components/NormalParagraph.jsx";
+import { HighlightedParagraph } from "../../components/HighlightedParagraph.jsx";
 
 export const FullmoonMP = () => {
     return (
         <div className="text-xl font-semibold">
             <div className="grid grid-cols-1 mb-6 text-center md:text-left text-2xl gap-7">
-                <div className="px-4 xl:px-0">
-                    A nőiség és a női közösség, a tánc (főleg a hastánc) régóta fontos része az életemnek.
+                <NormalParagraph text="A nőiség és a női közösség, a tánc (főleg a hastánc) régóta fontos része az életemnek.
                     Jól érzem magam női társaságban, szerintem óriási erőnk van külön-külön is, de még nagyobb együtt,
                     ha kölcsönös elfogadással, tisztelettel és szeretettel tudunk fordulni egymás felé.
                     Sokáig a táncban tudtam megélni ezt a kapcsolódást, harmóniát és erőt.
@@ -17,31 +18,22 @@ export const FullmoonMP = () => {
                     Akkor jöttem rá, hogy segíteni szeretnék. Segíteni, hogy minél több nő, család érezze a megtartó erőt,
                     azt, hogy van hova fordulniuk, ha kérdésük, kétségük van, meghallgatásra,
                     vagy csak egy kis praktikus segítségre vágynak. Tapasztaltam, és tapasztalom mindennap,
-                    hogy mekkora szükség van az értő figyelemre, naprakész, hiteles információkra, támogatásra.
-                </div>
-                <div className="px-4 xl:px-0">
-                    A saját élményeim, anyaságom óta szívügyemmé vált a női lét várandósság, szülés, gyermekágy,
-                    szoptatás körüli időszaka, és ebben az érzékeny időszakban való támogatás/segítés.
-                </div>
-                <div className="px-4 xl:px-0">
-                    Ezért 2020-ban elvégeztem az Anyatejút Tudásközpont Szoptatási Segítő Alaptanfolyamát, majd 2021-ben a
+                    hogy mekkora szükség van az értő figyelemre, naprakész, hiteles információkra, támogatásra." />
+                <NormalParagraph text="A saját élményeim, anyaságom óta szívügyemmé vált a női lét várandósság, szülés, gyermekágy,
+                    szoptatás körüli időszaka, és ebben az érzékeny időszakban való támogatás/segítés." />
+                <NormalParagraph text="Ezért 2020-ban elvégeztem az Anyatejút Tudásközpont Szoptatási Segítő Alaptanfolyamát, majd 2021-ben a
                     Középhaladó tanfolyamot is, valamint Békés Emőke Dúla tréningjét és műhelyét.
                     2021-ben részt vettem Miskovics-Kobza Zsuzsa Meső gyermekágyas segítő képzésén,
                     majd 2022-ben pre- és postnatal tréner is lettem. Nagy vágyam vált valóra, amikor a Fitmummy zenés tréning órák
                     megszervezésével összeért az életemnek az a két területe, ami a legjobban érdekel.
-                    A mozgás/tánc és az Édesanyák támogatása.
-                </div>
-                <div className="px-4 xl:px-0">
-                    Igyekszem folyamatosan továbbképezni magam, hogy minél több oldalról tudjam támogatni az
+                    A mozgás/tánc és az Édesanyák támogatása." />
+                <NormalParagraph text="Igyekszem folyamatosan továbbképezni magam, hogy minél több oldalról tudjam támogatni az
                     anyákat/családokat életük egyik legszebb, de ugyanakkor legnehezebb időszakában.
                     Saját önismereti utamon is haladok tovább, ami úgy gondolom mindenkinek fontos lenne,
                     de segítőként egyenesen létszükséglet. Ahhoz, hogy értő figyelemmel, ítélet- és kritikamentesen
                     tudjak odafordulni az Anyákhoz/családokhoz, tisztában kell lennem a saját piros gombjaimmal,
-                    hogy mi miért, hogy hat rám, mikor kapcsol be egy-egy történetben a saját élmény.
-                </div>
-                <div className="px-4 my-4 xl:px-0 text-center text-3xl text-primary font-bold">
-                    Nagyon jóleső érzés számomra adni, engem is feltölt, erőt ad. Tudom, hogy ez az utam!
-                </div>
+                    hogy mi miért, hogy hat rám, mikor kapcsol be egy-egy történetben a saját élmény." />
+                <HighlightedParagraph text="Nagyon jóleső érzés számomra adni, engem is feltölt, erőt ad. Tudom, hogy ez az utam!" />
             </div>
         </div>
     )
