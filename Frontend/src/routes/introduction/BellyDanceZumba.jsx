db1a1f4c2e73f395cc56f2783434de3f34747188
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-15 11:41:20 +0200

    Frontend: Styling; Add titles and qualifications in BellyDanceZumba intro page

--- Frontend/src/routes/introduction/BellyDanceZumba.jsx
+++ Frontend/src/routes/introduction/BellyDanceZumba.jsx
@@ -1,20 +1,25 @@
 import { NormalParagraph } from "../../components/NormalParagraph.jsx";
-import { HighlightedParagraph } from "../../components/HighlightedParagraph.jsx";
 import { HeroParagraph } from "../../components/HeroParagraph.jsx";
+import { ULParagraph } from "../../components/ULParagraph.jsx";
+import { Header } from "../../components/mainpage/Header.jsx";
 
 export const BellyDanceZumba = () => {
     return (
         <div className="text-xl font-semibold">
             <div className="grid grid-cols-1 mb-6 text-center md:text-left text-2xl gap-7">
+                <Header title="Ha táncolsz, élsz, ha élsz, táncolsz." classes="text-center" />
                 <HeroParagraph text="Gyerekkorom óta érdekelnek a különböző táncok,
                     mindig foglalkoztatott a színpad világa, az előadóművészet. Több táncműfajjal is megismerkedtem gyerekkoromban
                     (balett, néptánc, társastánc, modern tánc, cigány tánc, salsa, stb.),
                     de legjobban a Kelet varázslatos tánca ragadott meg."
+                               title="Beczkó Mónika - Mona Moon"
+                               titleExtraClasses="px-10 md:px-0"
                                imgPath="/src/assets/images/intro_bellydance_zumba_1.png"
                                imgAlt="Beczkó Mónika" imgRightSide={true}
                 />
                 <HeroParagraph text="A hastáncot 2005 januárjában kezdtem el, és azóta az életem egyik legfontosabb részévé vált.
                     Ezt a nőies táncműfajt Dina Jamilah ismertette meg velem az 1001 Éjszaka Hastánc Stúdióban."
+                               title="Kezdetek"
                                imgPath="/src/assets/images/intro_bellydance_zumba_2.png"
                                imgAlt="A kezdetek" imgRightSide={false}
                 />
@@ -22,12 +27,13 @@ export const BellyDanceZumba = () => {
                     Számos versenyt nyertünk csoportban, illetve szólóban is általában dobogós helyezéseket értem el.
                     Külföldi és hazai oktatók workshopjait is gyakran látogattam. Rendszeres fellépő voltam keleti éttermekben,
                     illetve privát fellépéseket is sokszor vállaltam születésnapokon, esküvőkön, stb."
+                               title="Versenyek és fellépések"
                                imgPath="/src/assets/images/intro_bellydance_zumba_3.png"
                                imgAlt="Versenyek és fellépések" imgRightSide={true}
                 />
-                <HighlightedParagraph text="Ha táncolsz, élsz, ha élsz, táncolsz." />
                 <HeroParagraph text="2008-tól kezdődően 3,5 éven keresztül tanítottam Dina tánciskolájában,
                     majd 2012-2013-ban vendégszerepeltem Olaszországban."
+                               title="Oktatás és külföld"
                                imgPath="/src/assets/images/intro_bellydance_zumba_4.png"
                                imgAlt="Oktatás és külföld" imgRightSide={false}
                 />
@@ -36,6 +42,7 @@ export const BellyDanceZumba = () => {
                     hogy a hastáncon kívül a Zumba fitnesszel is szeretnél foglalkozni. Ezért 2013 szeptemberében elvégeztem a
                     Zumba Fitness LLC. hivatalos Basic 1. képzését, majd később a Zumba Kids képzést is.
                     Ezután pedig termet béreltem, és elkezdtem a saját hastánc és Zumba óráim felépítését."
+                               title="Zumba az új szenvedély"
                                imgPath="/src/assets/images/intro_bellydance_zumba_5.png"
                                imgAlt="Zumba az új szenvedély" imgRightSide={true}
                 />
@@ -43,11 +50,14 @@ export const BellyDanceZumba = () => {
                     amiből nagyon sok újdonságot tanultam. Valamint Amaraya Társulatának tagjává váltam.
                     2-3 évig szerepeltem a társulattal különböző rendezvényeken, versenyeken, táncszínházi előadásokon.
                     Ez egy szuper izgalmas időszak volt az életemben."
+                               title="Táncszínház és az A-Dance"
                                imgPath="/src/assets/images/intro_bellydance_zumba_6.png"
                                imgAlt="Táncszínház és az A-Dance" imgRightSide={false}
                 />
                 <HeroParagraph text="2016-tól a tanítványaimnak is elkezdtem fellépő koreográfiákat készíteni, és fellépéseket vállalni.
                     Velük azóta is a Mona Moon és az Ezüst Csillagok néven működünk."
+                               title="Mona Moon és az Ezüst Csillagok"
+                               titleExtraClasses="px-8 md:px-0"
                                imgPath="/src/assets/images/intro_bellydance_zumba_7.png"
                                imgAlt="Mona Moon és az Ezüst Csillagok" imgRightSide={true}
                 />
@@ -55,12 +65,23 @@ export const BellyDanceZumba = () => {
                     illetve fellépésem sem volt. Majd az újrakezdés után nem sokkal kezdődött a covid, ami miatt átálltam online órákra.
                     Jelenleg is tartok online órákat, illetve egy ideje már a személyes órákat is újraindítottam.
                     2022 nyár elején pedig a hosszú kényszerpihenő után újra színpadra állhattunk."
+                               title="Napjaink"
                                imgPath="/src/assets/images/intro_bellydance_zumba_8.png"
                                imgAlt="Napjaink" imgRightSide={false}
                 />
                 <NormalParagraph text="A tánc adja az erőt a mindennapokhoz. Amikor táncolok, kinyílik a világ,
                     és ilyenkor vagyok igazán önmagam. Csodálatos dolog számomra a színpadon állni, adni a közönségnek magamból,
                     az energiámból a tánc univerzális nyelve által, és közben érezni a szeretetüket." extraClasses="font-bold" />
+                <ULParagraph title="Képesítések">
+                    <li>2006 - Orientális tánc szakvizsga - Országos Szórakoztatózenei Központ</li>
+                    <li>2008 - Sportoktató szakképesítés - SOTE TF</li>
+                    <li>2008 - Orientális tánc fakultáció - SOTE TF</li>
+                    <li>2013 - Hivatalos Zumba oktató, Basic steps level 1 - Zumba Fitness LLC</li>
+                    <li>2014 - Hivatalos Zumba Kids és Kids Jr. oktató - Zumba Fitness LLC</li>
+                    <li>2015 - Step by Step kezdő táncképzés - Amaraya</li>
+                    <li>2016 - Step by Step középhaladó táncképzés - Amaraya</li>
+                    <li>2016 - Első Lépés táncképzés - A-Dance</li>
+                </ULParagraph>
             </div>
         </div>
     )
