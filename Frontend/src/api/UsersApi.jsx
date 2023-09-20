8ac7a55a2c120ad55fd28f2cf8f3755d496897de
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-20 14:53:55 +0200

    Frontend: Add UsersApi.jsx with getAllUser and createUser functions

---
+++ Frontend/src/api/UsersApi.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { API_URL } from "../config/index.jsx";
+
+export class UsersApi {
+    constructor() {
+        if (this instanceof UsersApi) {
+            throw Error('A static class cannot be instantiated.');
+        }
+    }
+     static async getAllUser() {
+        const res = await axios.get(`${API_URL}/User`, { withCredentials : true });
+        return await res.data;
+    }
+
+    static async createUser(lastName, firstName, email, password) {
+        let errorMsg = "";
+        let errorStatusCode;
+
+        const res = await axios.post(`${API_URL}/User`, {
+            lastName: lastName,
+            firstName: firstName,
+            email: email,
+            password: password
+        }, { withCredentials : true })
+            .catch(error => {
+                errorStatusCode = error.response.status;
+                errorMsg = error.response.data;
+            });
+
+        return {
+            status: errorMsg ? errorStatusCode : res.status,
+            newUser: errorMsg ? null : res.data,
+            errorMsg: errorMsg ? errorMsg : null
+        };
+    }
+}
