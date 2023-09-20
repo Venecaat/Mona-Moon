a76d38624e699243238dd81cf0a03a8bbd2bcbfa
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-20 17:14:25 +0200

    Backend: Add AuthService with HashPw method

---
+++ Backend/Services/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Dtos.User;
+
+namespace Backend.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserService _userService;
+
+        public AuthService(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public RegisterUser HashPw(RegisterUser user)
+        {
+            string pwHash = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            user.Password = pwHash;
+            return user;
+        }
+    }
+}
---
+++ Backend/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Backend.Dtos.User;
+
+namespace Backend.Services
+{
+    public interface IAuthService
+    {
+        RegisterUser HashPw(RegisterUser user);
+    }
+}
