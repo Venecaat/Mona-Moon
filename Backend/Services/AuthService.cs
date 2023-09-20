ceb926bbc9a58021c4dc9068dbc6e5b8742dd0df
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-20 17:47:26 +0200

    Backend: Add Authenticate method in AuthService

--- Backend/Services/AuthService.cs
+++ Backend/Services/AuthService.cs
@@ -1,14 +1,18 @@
-﻿using Backend.Dtos.User;
+﻿using AutoMapper;
+using Backend.Database.Models.User;
+using Backend.Dtos.User;
 
 namespace Backend.Services
 {
     public class AuthService : IAuthService
     {
         private readonly IUserService _userService;
+        private readonly IMapper _mapper;
 
-        public AuthService(IUserService userService)
+        public AuthService(IUserService userService, IMapper mapper)
         {
             _userService = userService;
+            _mapper = mapper;
         }
 
         public RegisterUser HashPw(RegisterUser user)
@@ -16,6 +20,20 @@ namespace Backend.Services
             string pwHash = BCrypt.Net.BCrypt.HashPassword(user.Password);
             user.Password = pwHash;
             return user;
+        }
+
+        public async Task<PublicUser> Authenticate(LoginUser user)
+        {
+            User? resUser = await _userService.FindByEmail(user.Email);
+            bool verified = false;
+
+            if (resUser != null)
+            {
+                verified = BCrypt.Net.BCrypt.Verify(user.Password, resUser.Password);
+            }
+
+            if (resUser == null || !verified) return null;
+            return _mapper.Map<User, PublicUser>(resUser);
         }
     }
 }
--- Backend/Services/IAuthService.cs
+++ Backend/Services/IAuthService.cs
@@ -5,5 +5,6 @@ namespace Backend.Services
     public interface IAuthService
     {
         RegisterUser HashPw(RegisterUser user);
+        Task<PublicUser> Authenticate(LoginUser user);
     }
 }
