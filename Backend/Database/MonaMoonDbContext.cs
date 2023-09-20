93f94e97f1d694ba6edb7f7ad48e1ddb563f0d81
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-09-20 13:56:45 +0200

    Backend: Change email collation in User table

--- Backend/Database/MonaMoonDbContext.cs
+++ Backend/Database/MonaMoonDbContext.cs
@@ -10,5 +10,10 @@ namespace Backend.Database
         }
 
         public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            _ = modelBuilder.Entity<User>().Property(u => u.Email).UseCollation("SQL_Latin1_General_CP1_CS_AS"); // make field case-sensitive
+        }
     }
 }
