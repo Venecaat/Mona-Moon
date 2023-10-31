afdc6b0ac69041c67b3175cde5c6eb83a150cc5f
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-10-31 14:33:22 +0100

    BackendTests: Add tests for GetAllUser in UserControllerT

--- BackendTests/ControllerTests/UserControllerT.cs
+++ BackendTests/ControllerTests/UserControllerT.cs
@@ -6,6 +6,9 @@ using Backend.AutoMapper;
 using Backend.Database;
 using Backend.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Backend.Database.Models.User;
+using Backend.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BackendTests.ControllerTests
 {
@@ -24,11 +27,90 @@ namespace BackendTests.ControllerTests
             IMapper mapper = mapperConfig.CreateMapper();
 
             Mock<MonaMoonDbContext> mockContext = new(new DbContextOptions<MonaMoonDbContext>());
-            Mock<UserRepository> mockRepository = new Mock<UserRepository>(mockContext.Object);
+            Mock<UserRepository>  mockRepository = new Mock<UserRepository>(mockContext.Object);
             _userService = new Mock<UserService>(mockRepository.Object, mapper);
             _authService = new Mock<AuthService>(_userService.Object, mapper);
 
             _controller = new UserController(_userService.Object, _authService.Object);
+        }
+
+        [Test]
+        public void GetAllUser_ReturnsHttpStatusCode200()
+        {
+            var result = _controller.GetAllUser();
+            var statusCode = (result.Result.Result as ObjectResult)?.StatusCode;
+
+            int expectedStatusCode = 200;
+
+            Assert.That(expectedStatusCode, Is.EqualTo(statusCode));
+        }
+
+        [Test]
+        public void GetAllUser_ReturnsUsersList()
+        {
+            List<PublicUser> expectedUsers = new()
+            {
+                new ()
+                {
+                    Id = 1,
+                    Email = "totally@valid.com",
+                    FirstName = "Johnson",
+                    LastName = "Long",
+                    IsAdmin = false
+                },
+                new ()
+                {
+                    Id = 2,
+                    Email = "something@right.bs",
+                    FirstName = "Jelly",
+                    LastName = "Fish",
+                    IsAdmin = false
+                },
+                new ()
+                {
+                    Id = 3,
+                    Email = "marker123@free.net",
+                    FirstName = "Isaac",
+                    LastName = "Clarke",
+                    IsAdmin = true
+                },
+                new ()
+                {
+                    Id = 4,
+                    Email = "tryingFor@you.com",
+                    FirstName = "Yuki",
+                    LastName = "Onna",
+                    IsAdmin = false
+                },
+                new ()
+                {
+                    Id = 5,
+                    Email = "lastsamurai325@liamg.com",
+                    FirstName = "Oda",
+                    LastName = "Nobunaga",
+                    IsAdmin = false
+                }
+            };
+
+            _userService.Setup(x => x.GetAll()).Returns(Task.FromResult(expectedUsers));
+
+            var result = _controller.GetAllUser();
+            var resultUsers = (result.Result.Result as ObjectResult)?.Value;
+
+            Util.AreEqualByJson(expectedUsers, resultUsers);
+        }
+
+        [Test]
+        public void GetAllUser_ReturnsHttpStatusCode400()
+        {
+            _userService.Setup(x => x.GetAll()).Throws(new ArgumentNullException());
+
+            var result = _controller.GetAllUser();
+            var statusCode = (result.Result.Result as ObjectResult)?.StatusCode;
+
+            int expectedStatusCode = 400;
+
+            Assert.That(expectedStatusCode, Is.EqualTo(statusCode));
         }
     }
 }
