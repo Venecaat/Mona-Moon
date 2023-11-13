ccb68faa8b8b6eac036c2c68d7a51882d3c94f18
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-11-13 11:47:36 +0100

    BackendTests: Add tests for Authenticate method in AuthServiceT

--- BackendTests/ServiceTests/AuthServiceT.cs
+++ BackendTests/ServiceTests/AuthServiceT.cs
@@ -6,6 +6,7 @@ using Backend.Dtos.User;
 using Backend.Repositories;
 using Backend.Database;
 using Microsoft.EntityFrameworkCore;
+using Backend.Database.Models.User;
 
 namespace BackendTests.ServiceTests
 {
@@ -66,6 +67,107 @@ namespace BackendTests.ServiceTests
             RegisterUser user = _authService.HashPw(expectedUser);
 
             Util.AreEqualByJson(expectedUser, user);
+        }
+
+        [Test]
+        public async Task Authenticate_NotExistingUser_ReturnsNull()
+        {
+            LoginUser user = new LoginUser()
+            {
+                Email = "furina@fontain.com",
+                Password = "#Hydroarchon1"
+            };
+
+            _userService.Setup(x => x.FindByEmail(user.Email)).Returns(Task.FromResult<User?>(null));
+
+            PublicUser? resUser = await _authService.Authenticate(user);
+
+            Assert.Null(resUser);
+        }
+
+        [Test]
+        public async Task Authenticate_ExistingUserWithWrongPw_ReturnsNull()
+        {
+            string pw = "#Hydroarchon1";
+
+            RegisterUser newUser = new RegisterUser()
+            {
+                Email = "furina@fontain.com",
+                FirstName = "Test",
+                LastName = "User",
+                Password = pw
+            };
+
+            LoginUser user = new LoginUser()
+            {
+                Email = "furina@fontain.com",
+                Password = "#WrongPw123"
+            };
+
+            _authService.HashPw(newUser);
+
+            User existingUser = new User
+            {
+                Id = 1,
+                Email = "furina@fontain.com",
+                FirstName = "Furina",
+                LastName = "Lady",
+                Password = newUser.Password,
+                IsAdmin = true
+            };
+
+            _userService.Setup(x => x.FindByEmail(user.Email)).Returns(Task.FromResult<User?>(existingUser));
+
+            PublicUser? resUser = await _authService.Authenticate(user);
+
+            Assert.Null(resUser);
+        }
+
+        [Test]
+        public async Task Authenticate_ExistingUserWithCorrectPw_ReturnsUser()
+        {
+            string pw = "#Hydroarchon1";
+
+            RegisterUser newUser = new RegisterUser()
+            {
+                Email = "furina@fontain.com",
+                FirstName = "Furina",
+                LastName = "Lady",
+                Password = pw
+            };
+
+            LoginUser user = new LoginUser()
+            {
+                Email = "furina@fontain.com",
+                Password = pw
+            };
+
+            _authService.HashPw(newUser);
+
+            User existingUser = new User
+            {
+                Id = 325,
+                Email = "furina@fontain.com",
+                FirstName = "Furina",
+                LastName = "Lady",
+                Password = newUser.Password,
+                IsAdmin = true
+            };
+
+            PublicUser expectedUser = new PublicUser
+            {
+                Id = 325,
+                Email = "furina@fontain.com",
+                FirstName = "Furina",
+                LastName = "Lady",
+                IsAdmin = true
+            };
+
+            _userService.Setup(x => x.FindByEmail(user.Email)).Returns(Task.FromResult<User?>(existingUser));
+
+            PublicUser? resUser = await _authService.Authenticate(user);
+
+            Util.AreEqualByJson(expectedUser, resUser);
         }
     }
 }
